17fc936108dc625718d16ef50828ef59697b10cf
DuffVonAxel <85947269+DuffVonAxel@users.noreply.github.com>
2022-10-27 11:32:20 -0200

    ESP01. Atualizar.

--- mespXX_v04C.c
+++ mespXX_v04C.c
@@ -305,7 +305,6 @@ void espAPCfg(void)																// Configura o SoftAP.
 }
 
 /* 100% :) */
-
 unsigned char espApConectar(void)												// Conecta a um Access Point/HUB/Roteador.
 {	
 	unsigned char str020[]={"JAP"};
